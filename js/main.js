2b33ae02b7ea00f678a6d312965d410a50bf9090
techakhil-me <techakhilc47@gmail.com>
2024-04-01 16:53:22 +0530

    minor fixes

--- js/main.js
+++ js/main.js
@@ -80,19 +80,9 @@
 
     // products carousel
 
-
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    $('.back-to-top').fadeIn('slow');
+    
+    
 
 
     // Testimonials carousel
